25c98329b8e3920048e33b834d8cd5a643545505
Sukrit Banthao <arm.xdsk@hotmail.com>
2021-02-06 23:17:52 +0700

    refactor

---
+++ functions.py
@@ -0,0 +1,3 @@
+def evaluate(expr):
+    # todo: evaluate a given expression using some kind of API
+    pass
--- main.py
+++ main.py
@@ -1,60 +1,34 @@
-from kivy.app import App
 from kivy import require
-from kivy.uix.gridlayout import GridLayout
+from kivy.app import App
 from kivy.properties import ObjectProperty
+from kivy.uix.gridlayout import GridLayout
+
+from buttons import buttons, appending_operators, numbers_str
+from functions import *
 
 require("2.0.0")
 
 
 class DefaultPage(GridLayout):
     output_label = ObjectProperty(None)
-    appending_operators = ["+", "-", "x", "/"]
-    numbers_str = [str(e) for e in range(0, 10)]
-    buttons = {
-        "%": "%",
-        "CE": "CE",
-        "C": "C",
-        "<": "<",
-        "1/x": "1/x",
-        "x^2": "x^2",
-        "sqrt(x)": "sqrt(x)",
-        "/": "/",
-        "7": 7,
-        "8": 8,
-        "9": 9,
-        "x": "x",
-        "4": 4,
-        "5": 5,
-        "6": 6,
-        "-": "-",
-        "1": 1,
-        "2": 2,
-        "3": 3,
-        "+": "+",
-        "+/-": "+/-",
-        "0": 0,
-        ".": ".",
-        "=": "=",
-    }
 
     def on_release(self, button):
-        value = self.buttons[button.text]
-        if value in range(0, 10):
+        value = buttons[button.text]
+        if value in numbers_str:
             if self.output_label.text == "0":
                 self.output_label.text = str(value)
             else:
                 self.output_label.text += str(value)
-        elif value in self.appending_operators:
+        elif value in appending_operators:
             last = self.output_label.text[-1]
-            if last in self.numbers_str:
+            if last in numbers_str:
                 self.output_label.text += value
-            elif last in self.appending_operators:
+            elif last in appending_operators:
                 self.output_label.text = self.output_label.text[:-1] + value
         elif value == ".":
             pass
         else:
             pass
-
 
 
 class SimpleCalcApp(App):
